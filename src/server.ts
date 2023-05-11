ef43bb34111841396a5844d70f148b08da44289d
ahsun rahim <ahsunrahim@gmail.com>
2023-05-12 02:11:47 +0600

    mongoose practice

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+const port: number = 5000;
+
+// database connection
+
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/practice-mongoose");
+    console.log("📶 DataBase connection successfully");
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(`Failed to connect database ${error}`);
+  }
+}
+main();
